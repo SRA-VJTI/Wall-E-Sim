776019dba65c224e62da586480083b06f5a2a781
hashmis79 <hashmis79@yahoo.com>
2021-04-10 03:07:59 +0530

    Added Images

---
+++ Task_3/bin/CMakeFiles/Wall_E_CoppeliaSim.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/saad/Wall-E-CoppeliaSim/remote_api/extApi.c" "/home/saad/Wall-E-CoppeliaSim/Task_3/bin/CMakeFiles/Wall_E_CoppeliaSim.dir/home/saad/Wall-E-CoppeliaSim/remote_api/extApi.c.o"
+  "/home/saad/Wall-E-CoppeliaSim/remote_api/extApiPlatform.c" "/home/saad/Wall-E-CoppeliaSim/Task_3/bin/CMakeFiles/Wall_E_CoppeliaSim.dir/home/saad/Wall-E-CoppeliaSim/remote_api/extApiPlatform.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DO_NOT_USE_SHARED_MEMORY"
+  "MAX_EXT_API_CONNECTIONS=255"
+  "NON_MATLAB_PARSING"
+  "__linux"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../remote_api"
+  "../../src"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/saad/Wall-E-CoppeliaSim/Task_3/line_follow.cpp" "/home/saad/Wall-E-CoppeliaSim/Task_3/bin/CMakeFiles/Wall_E_CoppeliaSim.dir/line_follow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DO_NOT_USE_SHARED_MEMORY"
+  "MAX_EXT_API_CONNECTIONS=255"
+  "NON_MATLAB_PARSING"
+  "__linux"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../remote_api"
+  "../../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Task_3/bin/CMakeFiles/Wall_E_CoppeliaSim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wall_E_CoppeliaSim.dir/line_follow.cpp.o"
+  "CMakeFiles/Wall_E_CoppeliaSim.dir/home/saad/Wall-E-CoppeliaSim/remote_api/extApi.c.o"
+  "CMakeFiles/Wall_E_CoppeliaSim.dir/home/saad/Wall-E-CoppeliaSim/remote_api/extApiPlatform.c.o"
+  "Wall_E_CoppeliaSim.pdb"
+  "Wall_E_CoppeliaSim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Wall_E_CoppeliaSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
